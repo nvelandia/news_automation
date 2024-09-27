7b42900abd3e9ae8416ca96fd81530dc9fe35053
nvelandia <nvelandia97@gmail.com>
2024-09-27 17:46:59 -0300

    add channels

--- lib/lambdas/process-file/index.ts
+++ lib/lambdas/process-file/index.ts
@@ -1,29 +1,66 @@
 // @ts-ignore
-import { getS3Info } from './S3';
+const { getS3Info } = require('./S3');
+const { countries } = require('./channels');
+const AWS = require('aws-sdk');
+
+interface Icountry {
+  name: string;
+  text: string;
+}
 
 export const handler = async (event: any) => {
+  //
+  const stepfunctions = new AWS.StepFunctions();
   let matchInfo = await getS3Info(event);
-  console.log(matchInfo);
 
-  console.log('File uploaded:', matchInfo);
+  console.log('matchInfo', matchInfo);
 
-  // Configurar el ARN de la Step Function desde la variable de entorno
-  // const stateMachineArn = process.env.STATE_MACHINE_ARN;
+  if (!matchInfo) {
+    return;
+  }
 
-  // if (!stateMachineArn) {
-  //   throw new Error('STATE_MACHINE_ARN environment variable is not set');
+  // let tournament = validateCompetition(matchInfo?.match_channel);
+
+  // if (!tournament) {
+  //   return;
   // }
 
-  // // Iniciar la Step Function con los datos del evento S3
-  // const params = {
-  //   stateMachineArn: stateMachineArn,
-  //   input: JSON.stringify(event),
-  // };
+  // console.log('tournament', tournament);
 
-  // await stepfunctions.startExecution(params).promise();
+  // Parámetros para iniciar la Step Function
+  const params = {
+    stateMachineArn: process.env.STEP_FUNCTION_ARN, // ARN de la Step Function
+    input: JSON.stringify({
+      waitTime1: 10, // Puedes ajustar el tiempo de espera según sea necesario
+      waitTime2: 5, // Puedes ajustar el tiempo de espera según sea necesario
+    }),
+  };
 
-  // return {
-  //   statusCode: 200,
-  //   body: 'Step Function started!',
-  // };
+  try {
+    // Disparar la ejecución de la Step Function
+    const data = await stepfunctions.startExecution(params).promise();
+    console.log('Ejecución de Step Function iniciada:', data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify('Ejecución de Step Function iniciada con éxito!'),
+    };
+  } catch (error) {
+    console.error('Error al iniciar Step Function:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify('Error al iniciar Step Function'),
+    };
+  }
 };
+
+function validateCompetition(torneo: string) {
+  const result = countries.find((country: Icountry) =>
+    country.name.toLowerCase().includes(torneo.toLowerCase())
+  );
+
+  if (result) {
+    return { name: result.name, text: result.text };
+  } else {
+    return false;
+  }
+}
--- lib/stacks/news_automation-stack.ts
+++ lib/stacks/news_automation-stack.ts
@@ -1,3 +1,73 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Construct } from 'constructs';
+// import * as s3 from 'aws-cdk-lib/aws-s3';
+// import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+// import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+// import * as lambda from 'aws-cdk-lib/aws-lambda';
+// import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+// import * as path from 'path';
+
+// export class NewsAutomationStack extends cdk.Stack {
+//   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);
+
+//     // Environment
+//     const bucketName: string = process.env.BUCKET || 'dev';
+
+//     // Referenciar el bucket S3 existente
+//     const bucket = s3.Bucket.fromBucketName(this, 'ExistingBucket', bucketName);
+
+//     // Crear la Step Function
+//     const stateMachine = new sfn.StateMachine(this, 'StateMachine', {
+//       definition: new sfn.Pass(this, 'Start'), // Placeholder para definir los pasos luego
+//     });
+
+//     // Crear la Lambda que procesará el archivo
+//     const processFileLambda = new ProcessFileLambda(
+//       this,
+//       'ProcessFileLambda',
+//       stateMachine
+//     );
+
+//     // Crear el evento para activar la Lambda cuando se agregue un archivo en el prefijo "mam_test/"
+//     bucket.addEventNotification(
+//       s3.EventType.OBJECT_CREATED_PUT,
+//       new s3n.LambdaDestination(processFileLambda.lambdaFunction),
+//       { prefix: 'mam_test/' }
+//     );
+
+//     // Permisos para que S3 invoque la Lambda
+//     bucket.grantRead(processFileLambda.lambdaFunction);
+
+//     // Exportar el ARN de la State Machine
+//     new cdk.CfnOutput(this, 'StateMachineArn', {
+//       value: stateMachine.stateMachineArn,
+//     });
+//   }
+// }
+
+// export class ProcessFileLambda extends Construct {
+//   public readonly lambdaFunction: lambda.IFunction;
+
+//   constructor(scope: Construct, id: string, stateMachine: sfn.StateMachine) {
+//     super(scope, id);
+
+//     this.lambdaFunction = new lambda.Function(this, 'LambdaFunction', {
+//       runtime: lambda.Runtime.NODEJS_18_X,
+//       handler: 'index.handler',
+//       code: lambda.Code.fromAsset(
+//         path.join(__dirname, '..', 'lambdas/process-file')
+//       ),
+//       environment: {
+//         STATE_MACHINE_ARN: stateMachine.stateMachineArn,
+//       },
+//     });
+
+//     // Permisos para que la Lambda inicie la Step Function
+//     stateMachine.grantStartExecution(this.lambdaFunction);
+//   }
+// }
+
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
@@ -16,54 +86,101 @@ export class NewsAutomationStack extends cdk.Stack {
 
     // Referenciar el bucket S3 existente
     const bucket = s3.Bucket.fromBucketName(this, 'ExistingBucket', bucketName);
+    // Crear el evento para activar la Lambda cuando se agregue un archivo en el prefijo "mam_test/"
+
+    // Lambda one
+    const oneLambda = new lambda.Function(this, 'oneLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async () => {
+          console.log('lambda one');
+        };
+      `),
+    });
+
+    // Lambda two
+    const twoLambda = new lambda.Function(this, 'twoLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async () => {
+          console.log('lambda two');
+        };
+      `),
+    });
+
+    // Lambda three
+    const threeLambda = new lambda.Function(this, 'threeLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async () => {
+          console.log('lambda three');
+        };
+      `),
+    });
+
+    // Step 1: Lambda one
+    const oneLambdaTask = new tasks.LambdaInvoke(this, 'Invoke oneLambda', {
+      lambdaFunction: oneLambda,
+    });
+
+    // Step 2: Wait for the specified time (in seconds)
+    const waitTime1 = new sfn.Wait(this, 'Wait for time 1', {
+      time: sfn.WaitTime.secondsPath('$.waitTime1'),
+    });
+
+    // Step 3: Lambda two
+    const twoLambdaTask = new tasks.LambdaInvoke(this, 'Invoke twoLambda', {
+      lambdaFunction: twoLambda,
+    });
+
+    // Step 4: Wait for the specified time (in seconds)
+    const waitTime2 = new sfn.Wait(this, 'Wait for time 2', {
+      time: sfn.WaitTime.secondsPath('$.waitTime2'),
+    });
+
+    // Step 5: Lambda three
+    const threeLambdaTask = new tasks.LambdaInvoke(this, 'Invoke threeLambda', {
+      lambdaFunction: threeLambda,
+    });
+
+    // Definir la cadena de pasos de la Step Function
+    const definition = oneLambdaTask
+      .next(waitTime1)
+      .next(twoLambdaTask)
+      .next(waitTime2)
+      .next(threeLambdaTask);
 
     // Crear la Step Function
-    const stateMachine = new sfn.StateMachine(this, 'StateMachine', {
-      definition: new sfn.Pass(this, 'Start'), // Placeholder para definir los pasos luego
+    const stepFunction = new sfn.StateMachine(this, 'StepFunction', {
+      definition,
+      timeout: cdk.Duration.minutes(5),
     });
 
-    // Crear la Lambda que procesará el archivo
-    const processFileLambda = new ProcessFileLambda(
-      this,
-      'ProcessFileLambda',
-      stateMachine
-    );
-
-    // Crear el evento para activar la Lambda cuando se agregue un archivo en el prefijo "mam_test/"
-    bucket.addEventNotification(
-      s3.EventType.OBJECT_CREATED_PUT,
-      new s3n.LambdaDestination(processFileLambda.lambdaFunction),
-      { prefix: 'mam_test/' }
-    );
-
-    // Permisos para que S3 invoque la Lambda
-    bucket.grantRead(processFileLambda.lambdaFunction);
-
-    // Exportar el ARN de la State Machine
-    new cdk.CfnOutput(this, 'StateMachineArn', {
-      value: stateMachine.stateMachineArn,
-    });
-  }
-}
-
-export class ProcessFileLambda extends Construct {
-  public readonly lambdaFunction: lambda.IFunction;
-
-  constructor(scope: Construct, id: string, stateMachine: sfn.StateMachine) {
-    super(scope, id);
-
-    this.lambdaFunction = new lambda.Function(this, 'LambdaFunction', {
+    // Lambda listenBucket (disparadora de la Step Function)
+    const processFileLambda = new lambda.Function(this, 'LambdaFunction', {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: 'index.handler',
       code: lambda.Code.fromAsset(
         path.join(__dirname, '..', 'lambdas/process-file')
       ),
       environment: {
-        STATE_MACHINE_ARN: stateMachine.stateMachineArn,
+        STEP_FUNCTION_ARN: stepFunction.stateMachineArn, // Pasamos el ARN de la Step Function como variable de entorno
       },
     });
 
-    // Permisos para que la Lambda inicie la Step Function
-    stateMachine.grantStartExecution(this.lambdaFunction);
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED_PUT,
+      new s3n.LambdaDestination(processFileLambda),
+      { prefix: 'mam_test/' }
+    );
+
+    // Permisos para que S3 invoque la Lambda
+    bucket.grantRead(processFileLambda);
+
+    // Permisos para que la lambda listenBucket dispare la Step Function
+    stepFunction.grantStartExecution(processFileLambda);
   }
 }
